73683e6817a6eb22eb1c01012046197118edccb9
notusedusername <36472592+notusedusername@users.noreply.github.com>
2018-04-23 18:54:09 +0200

    Add files via upload

---
+++ valtozocsere_kul.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a = 5, b = 6;
+
+	cout<<"a= "<<a<<"\t b= "<<b<<"\n Csere..."<<endl;
+
+	a = a + b;
+
+	b = a -b;
+
+	a = a -b; 
+
+	cout<<"a= "<<a<<"\t b= "<<b<<endl;
+
+
+}
